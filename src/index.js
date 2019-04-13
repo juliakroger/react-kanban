7a30310616cb5c44cbd5f2ec5baa3da51642c11b
Julia Kroger <julia.kroger@gmail.com>
2019-04-13 20:25:12 -0300

    redux config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './store/reducers/reducers';
+
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+    , document.getElementById('root'));
 
 serviceWorker.unregister();
---
+++ src/store/reducers/reducers.js
@@ -0,0 +1,15 @@
+import * as act from '../actions/actionsTypes.js';
+
+const initialState = {
+
+};
+
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
