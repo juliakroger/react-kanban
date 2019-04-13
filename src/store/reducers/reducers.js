be4d5aedfad8041f0cfd0f96cd8055052247b5ce
Julia Kroger <julia.kroger@gmail.com>
2019-04-13 20:38:03 -0300

    adding initial state

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -1,7 +1,13 @@
 import * as act from '../actions/actionsTypes.js';
 
 const initialState = {
-
+  columns : [
+    { title: 'Backlog', tasks: [] },
+    { title: 'To do', tasks: [] },
+    { title: 'In Progress', tasks: [] },
+    { title: 'Testing', tasks: [] },
+    { title: 'Done', tasks: [] }
+  ]
 };
 
 const reducer = (state=initialState, action) => {
